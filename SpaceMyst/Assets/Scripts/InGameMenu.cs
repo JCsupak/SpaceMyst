e71b992ea42942f091204338b66522be4fb83d4c
James Csupak <jamescsupak@gmail.com>
2017-06-29 17:59:44 -0700

    Added main menu, game menu, and added some assets

---
+++ SpaceMyst/Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGameMenu : MonoBehaviour
+{
+
+    GameObject[] gameMenuObjects;
+    bool menuOpen = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameMenuObjects = GameObject.FindGameObjectsWithTag("GameMenu");
+        hideGameMenu();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!menuOpen)
+            {
+                showGameMenu();
+            }
+            else
+            {
+                hideGameMenu();
+            }
+        }
+	}
+
+    public void hideGameMenu()
+    {
+        menuOpen = false;
+        foreach(GameObject g in gameMenuObjects)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    public void showGameMenu()
+    {
+        menuOpen = true;
+        foreach(GameObject g in gameMenuObjects)
+        {
+            g.SetActive(true);
+        }
+    }
+
+}
